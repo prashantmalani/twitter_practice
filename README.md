5e9e8f6808c3e4b42594e9ba1bad5235981a5432
Prashant Malani <p.malani@gmail.com>
2013-12-10 00:56:37 -0800

    Initial setup
    Get the access_token and consumer key and secret
    from an app(Open Hatch Guevara trial).
    
    Created from username: guevara_twit_88
    
    Ensure that all the setup mentioned in
    https://openhatch.org/wiki/Twitter is done
    and that the skeleton code works as planned.
    
    Signed-off-by: Prashant Malani <p.malani@gmail.com>

--- README.md
+++
@@ -1,4 +0,0 @@
-twitter_practice
-================
-
-Practice project to understand using service APIs (taken from https://openhatch.org/wiki/Twitter)
---
+++ config.py
@@ -0,0 +1,7 @@
+#Please put the values here.
+key="YNOvcTlJzVJpXc3wOv7J0w"
+secret="E0fsQRcr0jQ3WkllUmkaznWDXm01QlZEyJamMQPscWY"
+access_key="2238834002-3eqL3ncA9AcVtMpOvUrQOPXba2UviEJC7gnt3Zn"
+access_secret="7TEQGHGucoXO63C1w4go6cl4PA8FrJqHxjGwAM5nlsFFR"
+
+
